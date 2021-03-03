5089e1aa2dbbf30b9997208adfdc809c2dae5e23
RobinsonLuzo <bendanielfagan@gmail.com>
2021-03-03 09:45:49 +0000

    Created layered nn. Simplified outputcalc.

--- neural_networks_pt2.jl
+++ neural_networks_pt2.jl
@@ -1,11 +1,23 @@
 # Neural Networks from Scratch Pt.2 -> Neural Network layer
 
+# We will create 4 inputs into 3 neurons -> as if it were the final layer.
 
-# Input to neuron:
+# Inputs to neurons:
 inputs  = [1, 2, 3, 2.5]
-weights = [0.2, 0.8, -0.5, 1.0]
-bias    = 2
 
-# resulting output from neuron
-output = inputs[1]*weights[1] + inputs[2]*weights[2] + inputs[3]*weights[3] + inputs[4]*weights[4] + bias
+weights1 = [0.2, 0.8, -0.5, 1.0]
+weights2 = [0.5, -0.91, 0.26, -0.5]
+weights3 = [-0.26, -0.27, 0.17, 0.87]
+
+bias1    = 2
+bias2    = 3
+bias3    = 0.5
+
+# resulting output from neurons
+neuron1_output = inputs[1]*weights1[1] + inputs[2]*weights1[2] + inputs[3]*weights1[3] + inputs[4]*weights1[4] + bias1
+neuron2_output = inputs[1]*weights2[1] + inputs[2]*weights2[2] + inputs[3]*weights2[3] + inputs[4]*weights2[4] + bias2
+neuron3_output = inputs[1]*weights3[1] + inputs[2]*weights3[2] + inputs[3]*weights3[3] + inputs[4]*weights3[4] + bias3
+
+# Final output
+output = [neuron1_output, neuron2_output, neuron3_output]
 println(output)
