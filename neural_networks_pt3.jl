f6a86f4787ca0a1d670986dcaf5363742c3fe6ce
RobinsonLuzo <bendanielfagan@gmail.com>
2021-03-08 15:14:21 +0000

    Hidden layer inital commit.

--- neural_networks_pt3.jl
+++ neural_networks_pt3.jl
@@ -1,6 +1,31 @@
 # Neural Networks from Scratch Pt.3 -> Dot Product
 # Also transitioning from lists into vectors and matricies.
 
+inputs  = [1, 2, 3, 2.5]
+
+weights = [0.2 0.8 -0.5 1.0;
+           0.5 -0.91 0.26 -0.5;
+           -0.26 -0.27 0.17 0.87]
+
+biases = [2, 3, 0.5]
+
+
+layer_outputs = []  # Output of current layer
+
+for (neuron_weights, neuron_bias) in zip(weights, biases)
+    neuron_output = 0   # Output of given neuron
+
+    for (n_input, weight) in zip(inputs, neuron_weights)
+        neuron_output += n_input*weight
+    end
+
+    neuron_output += neuron_bias
+    append!(layer_outputs, neuron_output)
+end
+
+println(layer_outputs)
+
+
 # The Dot Product multiplies vectors element-wise.
 # e.g. [1,2,3] [4,5,6] would be 1*4 + 2*5 + 3*6
 # In Julia the euqivilant of Numpy is LinearAlgebra
---
+++ neural_networks_pt5.jl
@@ -0,0 +1,24 @@
+# Neural Networks from Scratch Pt.2 -> Hidden Layer Activation Functions
+
+using LinearAlgebra
+using Random
+
+Random.seed!(0)
+
+X  = [1 2 3 2.5;
+    2.0 5.0 -1.0 2.0;
+    -1.5 2.7 3.3 -0.8]
+
+inputs = [0 2 -1 3.3 -2.7 1.1 2.2 -100]
+
+outputs = []
+
+for i in inputs
+    if i > 0
+        append!(outputs, i)
+    elseif i <= 0
+        append!(outputs, 0)
+    end
+end
+
+println(outputs)
